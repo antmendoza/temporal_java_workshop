01029eac115a8d75f5d6271b71a38203139b5ad7
Antonio Mendoza Pérez <antonio.perez@temporal.io>
2023-04-16 01:31:20 +0200

    wip

--- src/main/java/io/temporal/step1/moneytransferapp/workflow/activity/moneytransferapp/workflow/TransferRequest.java
+++
@@ -1,4 +0,0 @@
-package io.temporal.step1.moneytransferapp.workflow.activity.moneytransferapp.workflow;
-
-public record TransferRequest(String fromAccountId, String toAccountId, String referenceId, double amount) {
-}
