bdfda2c39f10a08e685e7dba11acb67710f57c15
Antonio Mendoza Pérez <antonio.perez@temporal.io>
2024-05-06 21:32:19 +0200

    documentation

--- prepare-your-environment.md
+++ prepare-your-environment.md
@@ -13,3 +13,8 @@ Follow the instructions [here](https://docs.temporal.io/cli#install) to install 
 Once you have Temporal CLI installed you can use the script [start-temporal-cli.sh](./start-temporal-cli.sh) to start the 
 server.
 
+
+
+After starting the Server, enable "Experimental mode" in the UI.
+
+![labs.png](labs.png)
---
+++ src/main/java/io/temporal/workshop/_0/demo/start-temporal-cli-with-db.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# If the script does not work for you, you can follow the instructions in "start-temporal-cli.md"
+
+: "${DEFAULT_NAMESPACE:=default}"
+
+: "${FILE_DB:=my_test.db}"
+
+# TODO add delete DB
+
+
+add_custom_search_attributes() {
+    until temporal operator search-attribute list --namespace "${DEFAULT_NAMESPACE}"; do
+      echo "Waiting for namespace..."
+      sleep 1
+    done
+
+    echo "Adding Custom  search attributes, only required for the final exercises "
+
+    temporal operator search-attribute create --namespace "${DEFAULT_NAMESPACE}" \
+        --name TransferRequestStatus --type Keyword
+
+}
+
+setup_server(){
+
+    sleep 2
+
+    add_custom_search_attributes
+
+    sleep 2
+
+    echo "------"
+    echo ">>> Temporal UI >>>  http://localhost:8080/"
+    echo "------"
+
+
+}
+
+# Run this func in parallel process. It will wait for server to start and then run required steps.
+setup_server &
+
+temporal server start-dev \
+--dynamic-config-value frontend.enableUpdateWorkflowExecution=true \
+--ui-port "8080" \
+--db-filename "${FILE_DB}" \
