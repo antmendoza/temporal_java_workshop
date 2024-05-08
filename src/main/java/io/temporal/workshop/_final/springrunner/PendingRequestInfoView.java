0898162272ab934c0ce78e40589b0580c10f5559
Antonio Mendoza Pérez <antonio.perez@temporal.io>
2024-05-08 09:24:43 +0200

    documentation

--- src/main/java/io/temporal/workshop/_final/springrunner/PendingRequestInfoView.java
+++ src/main/java/io/temporal/workshop/_final/springrunner/PendingRequestInfoView.java
@@ -3,4 +3,12 @@ package io.temporal.workshop._final.springrunner;
 import io.temporal.workshop.model.TransferRequest;
 
 public record PendingRequestInfoView(String workflowId, TransferRequest transferRequest) {
+
+
+    public String toReadableString() {
+        return
+                "WorkflowId='" + workflowId + '\'' +
+                        System.lineSeparator()+
+                transferRequest.toReadableString();
+    }
 }
